ba5ce2465c61b501e15d86b590cd33f20b4885ad
codeping <deping.du@gmail.com>
2020-06-17 23:17:01 +0800

    完善实现，使用容器中的 Consul 客户端。

---
+++ src/WebApiClient.Extensions.Consul.Demo/Api/ITestApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiClientCore.Attributes;
+
+namespace WebApiClient.Extensions.Consul.Demo
+{
+    public interface ITestApi
+    {
+        [HttpGet("/api/values")]
+        Task<string[]> Get();
+    }
+}
--- src/WebApiClient.Extensions.Consul/HttpApiServiceCollectionExtensions.cs
+++ src/WebApiClient.Extensions.Consul/HttpApiServiceCollectionExtensions.cs
@@ -14,34 +14,73 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class HttpApiServiceCollectionExtensions
     {
         public static IHttpClientBuilder AddHttpApiClient<THttpApi>(
-            this IServiceCollection services,
-            Action<HttpApiConsulOptions<THttpApi>> configureOptions)
-            where THttpApi : class
+               this IServiceCollection services, string consulServiceName)
+               where THttpApi : class
         {
-            return services.AddHttpApiClient<THttpApi>((optins, _) => configureOptions(optins));
+            return services.AddHttpApiClient<THttpApi>(
+                optins => optins.ConsulServiceName = consulServiceName);
         }
 
         public static IHttpClientBuilder AddHttpApiClient<THttpApi>(
             this IServiceCollection services,
-            Action<HttpApiConsulOptions<THttpApi>, IServiceProvider> configureOptions)
+            Action<HttpApiConsulOptions> configureOptions)
+            where THttpApi : class
+        {
+            return services.AddHttpApiClient<THttpApi>(
+                (optins, _) => configureOptions(optins));
+        }
+
+        public static IHttpClientBuilder AddHttpApiClient<THttpApi>(
+            this IServiceCollection services,
+            Action<HttpApiConsulOptions, IServiceProvider> configureOptions)
             where THttpApi : class
         {
             services
                 .AddHttpApi()
-                .AddOptions<HttpApiConsulOptions<THttpApi>>()
+                .AddOptions<HttpApiConsulOptions>()
                 .Configure(configureOptions);
 
             return services
                 .AddHttpClient(typeof(THttpApi).FullName)
-                .AddTypedClient((client, serviceProvider) =>
+                .AddTypedClient((client, provider) =>
                 {
-                    IOptions<HttpApiConsulOptions<THttpApi>> options = serviceProvider
-                        .GetRequiredService<IOptions<HttpApiConsulOptions<THttpApi>>>();
+                    IOptions<HttpApiConsulOptions> options = provider
+                        .GetRequiredService<IOptions<HttpApiConsulOptions>>();
 
-                    ServiceDiscoveryAsync(options.Value).Wait();
+                    provider.ServiceDiscoveryAsync(options.Value).Wait();
 
-                    return HttpApi.Create<THttpApi>(client, serviceProvider, options.Value);
+                    return HttpApi.Create<THttpApi>(client, provider, options.Value);
                 });
+        }
+
+        private static async Task ServiceDiscoveryAsync(
+            this IServiceProvider provider, HttpApiConsulOptions options)
+        {
+            IConsulClient consul = provider.GetRequiredService<IConsulClient>();
+
+            if (consul == null)
+            {
+                throw new InvalidOperationException(
+                    "没有找到 Consul 客户端，请使用 service.AddConsulClient(); 注入");
+            }
+
+            QueryResult<CatalogService[]> service = await consul.Catalog.Service(options.ConsulServiceName);
+
+            if (service.StatusCode == HttpStatusCode.OK && service.Response.Length > 0)
+            {
+                Random random = new Random(Environment.TickCount);
+
+                int index = random.Next(service.Response.Length);
+
+                CatalogService catalog = service.Response[index];
+
+                UriBuilder builder = new UriBuilder(catalog.ServiceAddress)
+                {
+                    Port = catalog.ServicePort,
+                };
+
+                options.HttpHost = builder.Uri;
+            }
         }
 
         private static IServiceCollection AddHttpApi(this IServiceCollection services)
@@ -52,38 +91,6 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<IKeyValueSerializer, KeyValueSerializer>();
             services.TryAddSingleton<IResponseCacheProvider, ResponseCacheProvider>();
             return services;
-        }
-
-        private static async Task ServiceDiscoveryAsync<THttpApi>(HttpApiConsulOptions<THttpApi> options)
-        {
-            if (options.ConculAddress != null && !string.IsNullOrWhiteSpace(options.ServiceName))
-            {
-                ConsulClient consul = new ConsulClient(config =>
-                {
-                    config.Address = options.ConculAddress;
-
-                    config.Datacenter = options.Datacenter;
-                });
-
-                QueryResult<CatalogService[]> service = await consul.Catalog.Service(options.ServiceName);
-
-                if (service.StatusCode == HttpStatusCode.OK && service.Response.Length > 0)
-                {
-                    Random random = new Random(Environment.TickCount);
-
-                    int index = random.Next(service.Response.Length);
-
-                    CatalogService catalog = service.Response[index];
-
-                    UriBuilder builder = new UriBuilder(catalog.ServiceAddress)
-                    {
-                        Port = catalog.ServicePort,
-                        Path = options.ServicePath,
-                    };
-
-                    options.HttpHost = builder.Uri;
-                }
-            }
         }
     }
 }
